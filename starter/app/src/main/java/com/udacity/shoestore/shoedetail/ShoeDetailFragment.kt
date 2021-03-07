17424cc1edf267d32a65fa8be173514abb8b5e91
Bruce <bruce@redravencomputing.com>
2021-03-07 13:14:42 -0700

    Fragment Setup
    Created skeletons for implementing fragments

---
+++ starter/app/src/main/java/com/udacity/shoestore/shoedetail/ShoeDetailFragment.kt
@@ -0,0 +1,22 @@
+package com.udacity.shoestore.shoedetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class ShoeDetailFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater, R.layout.fragment_shoe_detail, container, false)
+
+        return binding.root
+    }
+}
